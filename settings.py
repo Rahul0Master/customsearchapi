17ca9da57765f9d2f249e163aad046f887f2070d
Rahul_Acharya <104510324+Rahul0Master@users.noreply.github.com>
2024-03-10 17:02:54 +0530

    Update settings.py
    normal commit

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
-SEARCH_KEY = "AIzaSyC0P4wV3KIzYShkTJK004KrLkk8IvlA4L4"
-SEARCH_ID = "27bc11568bd38475b"
+SEARCH_KEY = "YOUR_API_KEY"
+SEARCH_ID = "cx"
 COUNTRY_RES= "countryIN"
 COUNTRY_CODE = "INDIA"
 SEARCH_URL = "https://www.googleapis.com/customsearch/v1?key={key}&cx={cx}&q={query}&start={start}&num=10&cr={country_res}&gl={country_code}"
